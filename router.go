d2f7f1df81583ebe1f5b08fd51d391a31432c230
Vinicius Correa <viniciuscorrea91@gmail.com>
2015-03-22 00:26:14 -0300

    Fixed handler bug

--- router.go
+++ router.go
@@ -1,15 +1,46 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
 )
 
 type Route struct {
 	Pattern       string
+	Method        string
 	GETHandler    func(w http.ResponseWriter, r *http.Request)
 	POSTHandler   func(w http.ResponseWriter, r *http.Request)
 	PUTHandler    func(w http.ResponseWriter, r *http.Request)
 	DELETEHandler func(w http.ResponseWriter, r *http.Request)
+}
+
+func (route Route) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch route.Method {
+	case "GET":
+		if route.GETHandler != nil {
+			route.GETHandler(w, r)
+		} else {
+			http.NotFound(w, r)
+		}
+	case "POST":
+		if route.POSTHandler != nil {
+			route.POSTHandler(w, r)
+		} else {
+			http.NotFound(w, r)
+		}
+	case "PUT":
+		if route.PUTHandler != nil {
+			route.PUTHandler(w, r)
+		} else {
+			http.NotFound(w, r)
+		}
+	case "DELETE":
+		if route.DELETEHandler != nil {
+			route.DELETEHandler(w, r)
+		} else {
+			http.NotFound(w, r)
+		}
+	}
 }
 
 type Routes []Route
@@ -17,20 +48,21 @@ type Routes []Route
 func (rs Routes) findOrNew(pattern string) Route {
 	for _, r := range rs {
 		if pattern == r.Pattern {
+			fmt.Printf("will return %v for pattern %s", r, pattern)
 			return r
 		}
 	}
 	return Route{Pattern: pattern}
 }
 
-func (rs Routes) insert(newRoute Route) {
-	for i, r := range rs {
+func (rs *Routes) save(newRoute Route) {
+	for i, r := range *rs {
 		if newRoute.Pattern == r.Pattern {
-			rs[i] = newRoute
+			(*rs)[i] = newRoute
 			return
 		}
 	}
-	routes = append(routes, newRoute)
+	*rs = append(routes, newRoute)
 }
 
 var routes Routes
@@ -43,6 +75,7 @@ func DefRoute(method string, pattern string, handler func(w http.ResponseWriter,
 	}
 
 	route := routes.findOrNew(pattern)
+	route.Method = method
 	switch method {
 	case "GET":
 		route.GETHandler = handler
@@ -53,40 +86,13 @@ func DefRoute(method string, pattern string, handler func(w http.ResponseWriter,
 	case "DELETE":
 		route.DELETEHandler = handler
 	}
-	routes.insert(route)
+	(&routes).save(route)
 }
 
 //RouteAll translates all the routes created with DefRoute to go's API. Calls GC after registering it
 func RouteAll() {
 	for _, route := range routes {
-		http.HandleFunc(route.Pattern, func(w http.ResponseWriter, r *http.Request) {
-			switch r.Method {
-			case "GET":
-				if route.GETHandler != nil {
-					route.GETHandler(w, r)
-				} else {
-					http.NotFound(w, r)
-				}
-			case "POST":
-				if route.POSTHandler != nil {
-					route.POSTHandler(w, r)
-				} else {
-					http.NotFound(w, r)
-				}
-			case "PUT":
-				if route.PUTHandler != nil {
-					route.PUTHandler(w, r)
-				} else {
-					http.NotFound(w, r)
-				}
-			case "DELETE":
-				if route.DELETEHandler != nil {
-					route.DELETEHandler(w, r)
-				} else {
-					http.NotFound(w, r)
-				}
-			}
-		})
+		http.Handle(route.Pattern, route)
 	}
 	//garbage collect
 	routes = nil
